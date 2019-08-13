9d2954a50cb48130e4a34e2b37719f4038c56057
ramustha <ramusthanest@gmail.com>
2019-08-13 11:34:16 +0700

    fixing

---
+++ client-feed/src/main/java/com/ramusthastudio/feed/controller/HomeController.java
@@ -0,0 +1,43 @@
+package com.ramusthastudio.feed.controller;
+
+import java.util.Map;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import static org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
+
+@Controller
+public class HomeController {
+  private final WebClient webClient;
+
+  public HomeController(WebClient aWebClient) {
+    webClient = aWebClient;
+  }
+
+  @GetMapping("/")
+  public String index(Model aModel, @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient aAuthorizedClient) {
+    aModel.addAttribute("userName", aAuthorizedClient.getPrincipalName());
+    aModel.addAttribute("clientName", aAuthorizedClient.getClientRegistration().getClientName());
+    return "index";
+  }
+
+  @GetMapping("/userinfo")
+  public String userinfo(Model aModel, @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient aAuthorizedClient) {
+    String userInfoEndpointUri = aAuthorizedClient.getClientRegistration()
+        .getProviderDetails().getUserInfoEndpoint().getUri();
+    Map userAttributes = webClient
+        .get()
+        .uri(userInfoEndpointUri)
+        .attributes(oauth2AuthorizedClient(aAuthorizedClient))
+        .retrieve()
+        .bodyToMono(Map.class)
+        .block();
+    aModel.addAttribute("userAttributes", userAttributes);
+    return "userinfo";
+  }
+
+}
